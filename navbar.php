8c5caa04471d7d59ea1ed7ae2b09ad0eeeb72a66
andrea-giambanco <andrea.giambanco@allievi.bearzi.it>
2023-03-24 12:21:59 +0100

    Correzione icone

--- navbar.php
+++ navbar.php
@@ -1,3 +1,7 @@
+<head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+</head>
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
   
   <div class="container-fluid">
--- profilo.php
+++ profilo.php
@@ -80,6 +80,8 @@
         integrity="sha256-2XFplPlrFClt0bIdPgpz8H7ojnk10H69xRqd9+uTShA=" crossorigin="anonymous" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+        <?php include "navbar.php"?>
 </head>
 
 <body>
