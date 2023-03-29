03f9c1b1a61249cb55cc98298832899b0d38b9b0
GiUsEpPeP00 <giuseppe.palermo@allievi.bearzi.it>
2023-03-29 12:55:15 +0200

    prova di list

--- index.php
+++ index.php
@@ -20,63 +20,8 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css"
         integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
-    <div class="container">
-        <!-- start event block -->
-        <div class="row align-items-center event-block no-gutters margin-40px-bottom">
-            <div class="col-lg-5 col-sm-12">
-                <div class="position-relative">
-                    <img src="https://www.bootdey.com/image/450x280/FFB6C1/000000" alt="">
-                    <div class="events-date">
-                        <div class="font-size28">10</div>
-                        <div class="font-size14">Mar</div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-7 col-sm-12">
-                <div class="padding-60px-lr md-padding-50px-lr sm-padding-30px-all xs-padding-25px-all">
-                    <h5
-                        class="margin-15px-bottom md-margin-10px-bottom font-size22 md-font-size20 xs-font-size18 font-weight-500">
-                        <a href="event-details.html" class="text-theme-color">Business Conference</a></h5>
-                    <ul class="event-time margin-10px-bottom md-margin-5px-bottom">
-                        <li><i class="far fa-clock margin-10px-right"></i> 01:30 PM - 04:30 PM</li>
-                        <li><i class="fas fa-user margin-5px-right"></i> Speaker : John Sminth</li>
-                    </ul>
-                    <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
-                        illum dolore</p>
-                    <a class="butn small margin-10px-top md-no-margin-top" href="event-details.html">Read More <i
-                            class="fas fa-long-arrow-alt-right margin-10px-left"></i></a>
-                </div>
-            </div>
-        </div>
-        <!-- end event block -->
+    <div class="container py-5 list">
 
-        <!-- start event block -->
-        <div class="row align-items-center event-block no-gutters margin-40px-bottom">
-            <div class="col-lg-7 order-2 order-lg-1">
-                <div class="padding-60px-lr md-padding-50px-lr sm-padding-30px-all xs-padding-25px-all">
-                    <h5
-                        class="margin-15px-bottom md-margin-10px-bottom font-size22 md-font-size20 xs-font-size18 font-weight-500">
-                        <a href="event-details.html" class="text-theme-color">Craft Workshops</a></h5>
-                    <ul class="event-time margin-10px-bottom md-margin-5px-bottom">
-                        <li><i class="far fa-clock margin-10px-right"></i> 09:00 AM - 09:30 PM</li>
-                        <li><i class="fas fa-user margin-5px-right"></i> Speaker : Alexa Zone</li>
-                    </ul>
-                    <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
-                        illum dolore</p>
-                    <a class="butn small margin-10px-top md-no-margin-top" href="event-details.html">Read More <i
-                            class="fas fa-long-arrow-alt-right margin-10px-left"></i></a>
-                </div>
-            </div>
-            <div class="col-lg-5 order-1 order-lg-2">
-                <div class="position-relative">
-                    <img src="https://www.bootdey.com/image/450x280/87CEFA/000000" alt="">
-                    <div class="events-date">
-                        <div class="font-size28">25 Febbraio</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- end event block -->
     </div>
 
     <!-- Code for fetch -->
@@ -94,6 +39,34 @@
             .then(response => response.text())
             .then(result => console.log(result))
             .catch(error => console.log('error', error));
+
+        
+            document.querySelector(".list").innerHTML +='<div class="row align-items-center event-block no-gutters margin-40px-bottom">
+                    <div class="col-lg-5 col-sm-12">
+                        <div class="position-relative">
+                            <img src="https://www.bootdey.com/image/450x280/FFB6C1/000000" alt="">
+                            <div class="events-date">
+                                <div class="font-size28">10</div>
+                                <div class="font-size14">Mar</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-7 col-sm-12">
+                        <div class="padding-60px-lr md-padding-50px-lr sm-padding-30px-all xs-padding-25px-all">
+                            <h5
+                                class="margin-15px-bottom md-margin-10px-bottom font-size22 md-font-size20 xs-font-size18 font-weight-500">
+                                <a href="event-details.html" class="text-theme-color">Business Conference</a></h5>
+                            <ul class="event-time margin-10px-bottom md-margin-5px-bottom">
+                                <li><i class="far fa-clock margin-10px-right"></i> 01:30 PM - 04:30 PM</li>
+                                <li><i class="fas fa-user margin-5px-right"></i> Speaker : John Sminth</li>
+                            </ul>
+                            <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
+                                illum dolore</p>
+                            <a class="butn small margin-10px-top md-no-margin-top" href="event-details.html">Read More <i
+                                    class="fas fa-long-arrow-alt-right margin-10px-left"></i></a>
+                        </div>
+                    </div>
+                </div>';
     </script>
 
 
