b2da13a3243b41bb26e8613ff471d37798941706
andrea-giambanco <andrea.giambanco@allievi.bearzi.it>
2023-04-05 11:57:23 +0200

    debug, continuazione pagine

--- chi_siamo.php
+++ chi_siamo.php
@@ -3,18 +3,19 @@
 include("navbar.php");
 
 ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Termini e Condizioni</title>
+    <title>Chi siamo</title>
 </head>
-
 <body>
-
+    
 </body>
 
 <footer>
@@ -22,5 +23,4 @@ include("navbar.php");
     include("footer.php");
     ?>
 </footer>
-
 </html>
