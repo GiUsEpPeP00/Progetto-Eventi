d2c37a14c91c240829b23997ba56ad25ef5d878d
andrea-giambanco <andrea.giambanco@allievi.bearzi.it>
2023-04-12 12:39:59 +0200

    termini page

--- termini.php
+++ termini.php
@@ -1,24 +1,77 @@
-<?php
-
-include("navbar.php");
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Termini e Condizioni</title>
+    <?php include 'navbar.php'; ?>
 </head>
 
+<body>
+    <h3 class="text-center mb-4 mt-2 pb-2 text-danger fw-bold">Termini e condizioni d'acquisto di TicketSud</h3>
+
+    <div class="container border border-dark rounded-5 p-4">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+            <div class="col">
+                <div class="mb-1">
+                    <strong>1- Accettazione dei termini e condizioni</strong>
+                </div>
+                <p>
+                    Questi termini e condizioni d'acquisto ("Termini") si applicano all'acquisto di qualsiasi biglietto,
+                    prodotto o servizio tramite il sito web TicketSud. Utilizzando il sito web TicketSud, l'utente
+                    accetta di essere vincolato dai presenti Termini e dalle politiche di TicketSud in merito alla
+                    privacy e alla sicurezza.
+                </p>
+
+
+            </div>
+            <div class="col">
+                <div class="mb-1">
+                    
+                        <strong>2- Acquisto di biglietti</strong>
+                    
+                </div>
+                <p>
+
+                    <strong>2.1</strong> TicketSud vende biglietti per eventi, concerti, spettacoli teatrali e sportivi
+                    di terze parti
+                    ("Eventi"). TicketSud non è il promotore o il responsabile dell'organizzazione degli Eventi, e non è
+                    responsabile di alcuna cancellazione, ritardo, modifica o altra questione relativa agli Eventi
+                    stessi.
+                    <br><strong>2.2 </strong> L'acquisto di biglietti tramite TicketSud è soggetto alla disponibilità
+                    dei
+                    biglietti per
+                    l'Evento desiderato.
+                </p>
+            </div>
+            <div class="col">
+                <p>
+                    <strong>3- Utilizzo del sito web TicketSud</strong><br>
+                    <strong>4.1</strong> L'utente si impegna a utilizzare il sito web TicketSud esclusivamente per scopi
+                    leciti.
+                    <br><strong>4.2</strong> L'utente si impegna a non utilizzare il sito web TicketSud per scopi
+                    fraudolenti o illegali.
+                    <br><strong>4.3</strong> L'utente si impegna a non utilizzare il sito web TicketSud per inviare
+                    messaggi di posta
+                    elettronica indesiderati o spam.
+                </p>
+            </div>
+            <div class="col">
+                <p>
+                    <strong>4- </strong> Accettazione dei termini e condizioni
+                    Questi termini e condizioni d'acquisto ("Termini") si applicano all'acquisto di qualsiasi biglietto,
+                    prodotto o servizio tramite il sito web TicketSud. Utilizzando il sito web TicketSud, l'utente
+                    accetta di essere vincolato dai presenti Termini e dalle politiche di TicketSud in merito alla
+                    privacy e alla sicurezza.
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
 <footer>
-    <?php
-    include("footer.php");
-    ?>
+    <?php include 'footer.php'; ?>
 </footer>
 
 </html>
